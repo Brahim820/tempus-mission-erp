ee542dae87f959fb52cdee9840112e0d22b670cf
ab.salhi <ab.salhi@324b3cef-64c2-7ec0-5618-98e7ac8a82dc>
2014-02-22 11:50:57 +0000

    first commit test

--- trunk/extra-addons/Mission_UH1/__init__.py
+++ trunk/extra-addons/Mission_UH1/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#
+#	test by salhi
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (http://tiny.be). All Rights Reserved
 #    
